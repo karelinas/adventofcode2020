85fe3cd2c6139e4d58427da2f60bcc6a8bf7764f
karelinas <45606302+karelinas@users.noreply.github.com>
2020-12-04 17:53:28 +0200

    Day 04: refactor away big if-else in validation

--- day04/main.py
+++ day04/main.py
@@ -9,66 +9,81 @@ Run with:
     python3 main.py < input.txt
 """
 
-from sys import stdin
+from collections import defaultdict
 from itertools import takewhile
+from sys import stdin
+
+
+class IntValidator:
+    def __init__(self, minvalue, maxvalue):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
+    def __call__(self, value):
+        value = int(value)
+        return value >= self.minvalue and value <= self.maxvalue
+
+
+class HeightValidator:
+    def __call__(self, value):
+        if value.endswith('cm'):
+            return IntValidator(150, 193)(value[:-2])
+        elif value.endswith('in'):
+            return IntValidator(59, 76)(value[:-2])
+        return False
+
+
+class HairColorValidator:
+    allowed_characters = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                          'a', 'b', 'c', 'd', 'e', 'f')
+
+    def __call__(self, value):
+        return (len(value) == 7 and value[0] == '#' and
+                all(ch in self.allowed_characters for ch in value[1:]))
+
+
+class EyeColorValidator:
+    valid_eyecolors = ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
+
+    def __call__(self, value):
+        return value in self.valid_eyecolors
+
+
+class PassportIdValidator:
+    allowed_characters = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+    def __call__(self, value):
+        return len(value) == 9 and all(ch in self.allowed_characters for ch in value)
+
+
+class CountryIdValidator:
+    def __call__(self, value):
+        return True
+
+
+class DefaultValidator:
+    def __call__(self, value):
+        return False
 
 
 def fields_exist(passport, required_fields):
     return all(field in passport for field in required_fields)
 
 
-def valid_int(string, minval, maxval):
-    value = int(string)
-    return value >= minval and value <= maxval
-
-
-def valid_height(string):
-    if string.endswith('cm'):
-        return valid_int(string[:-2], 150, 193)
-    elif string.endswith('in'):
-        return valid_int(string[:-2], 59, 76)
-    return False
-
-
-def valid_hair_color(string):
-    allowed_characters = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                          'a', 'b', 'c', 'd', 'e', 'f')
-    return (len(string) == 7 and string[0] == '#' and
-            all(ch in allowed_characters for ch in string[1:]))
-
-
-def valid_eye_color(string):
-    return string in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
-
-
-def valid_pid(string):
-    allowed_characters = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    return len(string) == 9 and all(ch in allowed_characters for ch in string)
-
-
-def valid_field(field, value):
-    if field == 'byr':
-        return valid_int(value, 1920, 2002)
-    elif field == 'iyr':
-        return valid_int(value, 2010, 2020)
-    elif field == 'eyr':
-        return valid_int(value, 2020, 2030)
-    elif field == 'hgt':
-        return valid_height(value)
-    elif field == 'hcl':
-        return valid_hair_color(value)
-    elif field == 'ecl':
-        return valid_eye_color(value)
-    elif field == 'pid':
-        return valid_pid(value)
-    elif field == 'cid':
-        return True
-    return False
-
-
 def valid_passport(passport, required_fields):
+    validators = defaultdict(DefaultValidator,
+        {
+            'byr': IntValidator(1920, 2002),
+            'iyr': IntValidator(2010, 2020),
+            'eyr': IntValidator(2020, 2030),
+            'hgt': HeightValidator(),
+            'hcl': HairColorValidator(),
+            'ecl': EyeColorValidator(),
+            'pid': PassportIdValidator(),
+            'cid': CountryIdValidator()
+        }
+    )
     return (fields_exist(passport, required_fields) and
-            all(valid_field(key, val) for key, val in passport.items()))
+            all(validators[key](val) for key, val in passport.items()))
 
 
 def passport(data):
