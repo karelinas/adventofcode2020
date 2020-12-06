2b2d1dc45c9c94d7a65448dc7e0b8a9b00b313cb
karelinas <45606302+karelinas@users.noreply.github.com>
2020-12-06 15:48:50 +0200

    Day 06: full solution

---
+++ day06/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Full solution for Advent of Code 2020, day 06
+
+Run with:
+
+    python3 main.py < input.txt
+"""
+
+from sys import stdin
+
+answers = [
+        list(set(yes for yes in person.strip()) for person in group.split())
+        for group in stdin.read().split('\n\n')
+]
+
+print("Part one:", sum(len(set.union(*group)) for group in answers))
+print("Part two:", sum(len(set.intersection(*group)) for group in answers))
