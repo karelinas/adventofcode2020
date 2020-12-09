ba6f36326ce30c3c16f722d843bb754da0f45328
karelinas <45606302+karelinas@users.noreply.github.com>
2020-12-10 01:25:06 +0200

    Day 08: use namedtuple for instruction
    inst.op and inst.arg are clearer than inst[0] and inst[1].

--- day08/main.py
+++ day08/main.py
@@ -9,9 +9,11 @@ Usage:
     python3 main.py < input.txt
 """
 
+from collections import namedtuple
 from re import match
 from sys import stdin
 
+Instruction = namedtuple('Instruction', ['op', 'arg'])
 
 class Cpu:
     instructions = {
@@ -26,13 +28,13 @@ class Cpu:
         seen = set()
         while pc < len(program) and pc not in seen:
             seen.add(pc)
-            pc, acc = cls.instructions[program[pc][0]](pc, acc, program[pc][1])
+            pc, acc = cls.instructions[program[pc].op](pc, acc, program[pc].arg)
         return (pc not in seen, acc)
 
 
 def instruction(line):
-    operator, operand = match(r'^(\w+) ([+-]*\d+)$', line).groups()
-    return (operator, int(operand))
+    op, arg = match(r'^(\w+) ([+-]*\d+)$', line).groups()
+    return Instruction(op, int(arg))
 
 
 def swap_operator(ops, pos):
@@ -41,7 +43,7 @@ def swap_operator(ops, pos):
         'nop': 'jmp',
         'acc': 'acc'
     }
-    return ops[:pos] + [(swap_map[ops[pos][0]], ops[pos][1])] + ops[pos+1:]
+    return ops[:pos] + [Instruction(swap_map[ops[pos].op], ops[pos].arg)] + ops[pos+1:]
 
 
 program = [instruction(line) for line in stdin]
