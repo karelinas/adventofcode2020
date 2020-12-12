caf2e5d9ab2ef3b69e49b01fa638520466120bea
karelinas <45606302+karelinas@users.noreply.github.com>
2020-12-12 23:59:16 +0200

    Day 12: formatting and style clean ups

--- day12/main.py
+++ day12/main.py
@@ -11,40 +11,41 @@ Usage:
 
 from sys import stdin
 
+
 def manhattan_distance(z):
     return int(abs(z.real) + abs(z.imag))
 
+
 DIRECTIONS = {
-    'N':  1j, # ( 0,  1)
-    'S': -1j, # ( 0, -1)
-    'W': -1,  # (-1,  0)
-    'E':  1   # ( 1,  0)
+    'N': complex( 1j), # ( 0,  1)
+    'S': complex(-1j), # ( 0, -1)
+    'W': complex(-1 ), # (-1,  0)
+    'E': complex( 1 )  # ( 1,  0)
 }
 
 ROTATIONS = {
-    'L':  1j,
-    'R': -1j
+    'L': complex( 1j),
+    'R': complex(-1j)
 }
 
 instructions = map(lambda x: (x[0], int(x[1:])), stdin)
 
-# For part one
-facing = complex(DIRECTIONS['E'])
-pos1 = complex()
-# For part two
-waypoint = 10+1j
-pos2 = complex()
+facing = DIRECTIONS['E']  # part 1
+pos1   = 0j               # part 1
+
+waypoint = 10+1j # part 2
+pos2     = 0j    # part 2
 
 for action, value in instructions:
     if action in DIRECTIONS:
-        pos1 = pos1 + value * complex(DIRECTIONS[action])
-        waypoint = waypoint + value * complex(DIRECTIONS[action])
+        pos1     = pos1     + value * DIRECTIONS[action]  # part 1
+        waypoint = waypoint + value * DIRECTIONS[action]  # part 2
     elif action in ROTATIONS:
-        facing = facing * (ROTATIONS[action] ** (value // 90))
-        waypoint = waypoint * (ROTATIONS[action] ** (value // 90))
+        facing   = facing   * ROTATIONS[action] ** (value // 90)  # part 1
+        waypoint = waypoint * ROTATIONS[action] ** (value // 90)  # part 2
     elif action == 'F':
-        pos1 = pos1 + facing * value
-        pos2 = pos2 + waypoint * value
+        pos1 = pos1 + facing   * value  # part 1
+        pos2 = pos2 + waypoint * value  # part 2
 
 print("Part one:", manhattan_distance(pos1))
 print("Part two:", manhattan_distance(pos2))
