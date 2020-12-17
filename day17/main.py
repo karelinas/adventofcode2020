ceeeacda095f6e53ed15319d49685bc9adb5c591
karelinas <45606302+karelinas@users.noreply.github.com>
2020-12-17 21:25:38 +0200

    Day 17: readability improvements
    * split adjacent() into functions for readability
    * refactor active cube resolution into one list comprehension

--- day17/main.py
+++ day17/main.py
@@ -15,13 +15,21 @@ from operator import add
 from sys import stdin
 
 
+def vector_add(v1, v2):
+    return tuple(map(add, v1, v2))
+
+
+def adjacency_vectors(dimension):
+    return product(*[[-1, 0, 1] for _ in range(dimension)])
+
+
 def adjacent(vec):
     origin = tuple(0 for _ in range(len(vec)))
-    return set(
-        tuple(map(add, vec, delta))
-        for delta in product(*[[-1, 0, 1] for _ in range(len(vec))])
+    return {
+        vector_add(vec, delta)
+        for delta in adjacency_vectors(len(vec))
         if delta != origin
-    )
+    }
 
 
 def simulate(active_cubes):
@@ -32,16 +40,15 @@ def simulate(active_cubes):
             neighbors[pos] += 1
 
     # Resolve active cubes
-    new_cubes = set()
-    for pos, count in neighbors.items():
-        if count == 3:
-            new_cubes.add(pos)
-        elif count == 2 and pos in active_cubes:
-            new_cubes.add(pos)
-    return new_cubes
+    return {
+        pos
+        for pos, count in neighbors.items()
+        if count == 3 or (count == 2 and pos in active_cubes)
+    }
 
 
 if __name__ == '__main__':
+    # Read input
     CUBES2D = {
         (x, y)
         for y, line in enumerate(stdin)
@@ -53,10 +60,10 @@ if __name__ == '__main__':
     cubes3d = {(x, y, 0) for x, y in CUBES2D}
     for _ in range(6):
         cubes3d = simulate(cubes3d)
-    print("part one:", len(cubes3d))
+    print("Part one:", len(cubes3d))
 
     # Part 2: 4D simulation
     cubes4d = {(x, y, 0, 0) for x, y in CUBES2D}
     for _ in range(6):
         cubes4d = simulate(cubes4d)
-    print("part two:", len(cubes4d))
+    print("Part two:", len(cubes4d))
